50acc10312161a8e967fe6f4fdb760d7b606c938
SatisKia <satis@msd.biglobe.ne.jp>
2022-03-29 11:26:19 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import 'test.dart';
+import 'widget.dart';
 
 void main() {
   runApp(const MyApp());
